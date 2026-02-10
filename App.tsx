10dc20d9ea01f4ce9610427b6a5c68edc00369be
kamalalshinawi <kamalalshinawi@gmail.com>
2026-02-10 22:24:09 +0200

    Create SocialContantsUser and create a UserAvatar

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import IntroScreen from "./src/screens/IntroScreen";
+import SocialContactUser from "./src/screens/SocialContactUser";
 
 export default function App() {
   return (
+    
     <View style={styles.container}>
-      <IntroScreen />
+      {/* <IntroScreen /> */}
+      < SocialContactUser />
     </View>
   );
 }
---
+++ src/screens/SocialContactUser.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import UserAvatar from '../components/UserAvatar'
+
+const SocialContactUser = () => {
+  return (
+    <View>
+      <UserAvatar />
+    </View>
+  )
+}
+
+export default SocialContactUser
+
+const styles = StyleSheet.create({})
