b359b348b28fe95fa391e18c1a2101255d715fc0
kamalalshinawi <kamalalshinawi@gmail.com>
2026-02-11 22:59:06 +0200

    customize topTap with useState hook

--- src/components/TopTap.tsx
+++ src/components/TopTap.tsx
@@ -1,14 +1,64 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { s, vs } from "react-native-size-matters";
+import React, { useState } from "react";
 
+const Active_BG = "#75563B";
+const Active_txt = "#fff";
+const inActive_txt = "#2C2016";
 const TopTap = () => {
+  type Tabs = string;
+  const Tabs: Tabs[] = ["Live", "Reloaded"];
+  const [active, setactive] = useState("Live");
+
+  // const BGActive = backgroundColor: "#75563B";
   return (
-    <View>
-      <Text>TopTap</Text>
+    <View style={styles.container}>
+      {Tabs.map((tabName) => {
+        const ifActive = active === tabName;
+        return (
+          <TouchableOpacity
+            style={[
+              styles.tabButton,
+              ifActive && { backgroundColor: Active_BG },
+            ]}
+            onPress={() => setactive(tabName)}
+          >
+            <Text style={ifActive ? styles.ActiveTxt : styles.inActiveTxt}>
+              {tabName}
+            </Text>
+          </TouchableOpacity>
+        );
+      })}
     </View>
-  )
-}
+  );
+};
 
-export default TopTap
+export default TopTap;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    height: vs(48),
+    backgroundColor: "#F5F5F4",
+    borderRadius: s(12),
+    flexDirection: "row",
+    alignItems: "center",
+    padding: s(4),
+  },
+  tabButton: {
+    height: vs(32),
+    flex: 1,
+    borderRadius: s(8),
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  ActiveTxt: {
+    color: Active_txt,
+    fontSize: s(14),
+    fontWeight: "semibold",
+  },
+  inActiveTxt: {
+    color: inActive_txt,
+    fontSize: s(14),
+    fontWeight: "semibold",
+  },
+});
